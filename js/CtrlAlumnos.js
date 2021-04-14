b2e05f5fc8ddc830857eaa7b66ba483f4d103044
proyecto42vglm <82478414+proyecto42vglm@users.noreply.github.com>
2021-04-13 21:51:33 -0500

    Update CtrlAlumnos.js

--- js/CtrlAlumnos.js
+++ js/CtrlAlumnos.js
@@ -68,6 +68,9 @@ function htmlFila(doc) {
   const data = doc.data();
   const matricula = cod(data.matricula);
   const nombre = cod(data.nombre);
+  const nombre = cod(data.telefono);
+  const nombre = cod(data.grupo);
+  const nombre = cod(data.fecha);
   const parámetros =
     new URLSearchParams();
   parámetros.append("id", doc.id);
