9af94e7d119779480181bc895d174c44086d88c0
proyecto42vglm <82478414+proyecto42vglm@users.noreply.github.com>
2021-04-13 20:17:24 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,7 +8,7 @@
  * servidor. */
 // @ts-ignore 
 firebase.initializeApp({
-    piKey: "AIzaSyCYldxbi6104J1WZrMx0ImlkGLGivL0L_I",
+    apiKey: "AIzaSyCYldxbi6104J1WZrMx0ImlkGLGivL0L_I",
     authDomain: "proyecto42lmvg-6fd85.firebaseapp.com",
     projectId: "proyecto42lmvg-6fd85",
     storageBucket: "proyecto42lmvg-6fd85.appspot.com",
