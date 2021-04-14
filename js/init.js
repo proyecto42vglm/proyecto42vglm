088ab40868803c8585c34c04a5d46c479b3f7b21
proyecto42vglm <82478414+proyecto42vglm@users.noreply.github.com>
2021-04-13 20:11:20 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore 
 firebase.initializeApp({
-    apiKey: "AIzaSyCYldxbi6104J1WZrMx0ImlkGLGivL0L_I",
+    piKey: "AIzaSyCYldxbi6104J1WZrMx0ImlkGLGivL0L_I",
     authDomain: "proyecto42lmvg-6fd85.firebaseapp.com",
     projectId: "proyecto42lmvg-6fd85",
     storageBucket: "proyecto42lmvg-6fd85.appspot.com",
     messagingSenderId: "610646727123",
-    appId: "1:610646727123:web:1b449b65b7ca9ca11b3726",
-    measurementId: "G-829B2LY36E
+    appId: "1:610646727123:web:07de1e74e2797a161b3726",
+    measurementId: "G-2V339QNEPL"
 });
